b2eff0786ebccb84c90f2a7dff3d2786f48d6496
BillH <wmhhardgroveiv@gmail.com>
2022-06-23 17:16:59 -0400

    index page questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,147 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+const generateMarkdownPage = require("utils/generateMarkdown.js");
+
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+        return inquirer.prompt([
+      {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?",
+        validate: nameInput => {
+          if (nameInput) {
+              return true;
+          } else {
+              console.log('Please enter your title!');
+              return false; 
+          }
+        }
+      },
+      {
+        type: "input",
+        message: "What is the description of your project:",
+        name: "description",
+        validate: nameInput => {
+          if (nameInput) {
+              return true;
+          } else {
+              console.log('Please enter your description!');
+              return false; 
+          }
+        }
+      },
+      {
+        type: "list",
+        name: "license",
+        message: "Please select the license you used for this project.",
+        choices: [
+          "Mozilla",
+          "MIT",
+        ],
+        validate: nameInput => {
+          if (nameInput) {
+              return true;
+          } else {
+              console.log('Please select your licence!');
+              return false; 
+          }
+        }
+      },
+      {
+        type: "input",
+        name: "author",
+        message: "What is your name?",
+        validate: nameInput => {
+          if (nameInput) {
+              return true;
+          } else {
+              console.log('Please enter your name!');
+              return false; 
+          }
+        }
+      },
+      {
+        type: "input",
+        name: "userName",
+        message: "What is your GitHub username?",
+        validate: nameInput => {
+          if (nameInput) {
+              return true;
+          } else {
+              console.log('Please enter your GitHub user name!');
+              return false; 
+          }
+        }
+      },
+      {
+        type: "input",
+        name: "userEmail",
+        message: "What is your email?",
+        validate: nameInput => {
+          if (nameInput) {
+              return true;
+          } else {
+              console.log('Please enter your email!');
+              return false; 
+          }
+        }
+      },
+      {
+        type: "input",
+        name: "URL",
+        message: "What is the URL of the live site?",
+        validate: nameInput => {
+          if (nameInput) {
+              return true;
+          } else {
+              console.log('Please enter your URL!');
+              return false; 
+          }
+        }
+      },
+      {
+        type: "input",
+        name: "repo",
+        message: "What is the URL of the github repo?",
+        validate: nameInput => {
+          if (nameInput) {
+              return true;
+          } else {
+              console.log('Please enter your GitHub repo!');
+              return false; 
+          }
+        }
+      }
+    ]);
+  }
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(data) {
+  fs.writeFile('README.md', data, err => {
+    if (err) {
+        console.log(err);
+        return;
+    } else {
+        console.log("You have a README!")
+    }
+})
+}; 
+
 
 // TODO: Create a function to initialize app
-function init() {}
 
-// Function call to initialize app
-init();
+questions()
+.then(answers => {
+    return generateMarkdownPage(answers);
+})
+.then(data => {
+    return writeToFile(data);
+})
+.catch(err => {
+    console.log(err)
+})
